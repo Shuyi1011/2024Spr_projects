fba60c8f2efb0ab9e4556fb38ed8a2b518df3850
Shuyi1011 <guoshuyi1011@gmail.com>
2024-04-29 20:29:29 -0500

    refine data process tools

--- utils.py
+++ utils.py
@@ -1,14 +1,18 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+import os
 
 # Data process tools
+def get_all_file_paths(directory):
+    all_files = os.listdir(directory)
+    all_csv_files = [file for file in all_files if file.endswith(".csv")]
+    all_csv_file_paths = [os.path.join(directory, file) for file in all_csv_files]
+    return all_csv_file_paths
+
 def check_columns(file_path, required_columns):
-    qualified_files = []
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(file_path, nrows=1)
     if set(required_columns).issubset(df.columns):
-        file_id = file_path.split("/")[-1]
-        qualified_files.append(file_id)
-        print(qualified_files)
-        return qualified_files
+        return file_path
 
 # Data visualization tools
 def plot_immigration_over_time(data, regions, mutiple=True):
